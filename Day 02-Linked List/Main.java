f009c48924a6704c9b73a6f94ff41f0aebdf18f5
lakshay1906 <lbhati020@gmail.com>
2025-05-28 19:28:44 +0530

    Interview question of Linked List

--- Day 02-Linked List/Main.java
+++ Day 02-Linked List/Main.java
@@ -1,27 +1,47 @@
 
 import linkedlist.*;
-import linkedlist.LinkedList.Node;
 
 class Main {
 
     public static void main(String args[]) {
         System.err.println("Radhe Radhe");
-        LinkedList myLinkedList = new LinkedList(3);
-        myLinkedList.prepend(2);
-        myLinkedList.prepend(1);
-        myLinkedList.append(4);
+        LinkedList myLinkedList = new LinkedList(1);
+        // myLinkedList.prepend(2);
+        // myLinkedList.prepend(1);
+        // myLinkedList.append(0);
+        // myLinkedList.append(0);
+        // myLinkedList.append(0);
+        myLinkedList.append(3);
         myLinkedList.append(5);
-        myLinkedList.removeFirst();
-        Node deletedNode = myLinkedList.removeLast();
-        System.err.println("DeletedNode: " + deletedNode.value);
+        myLinkedList.append(2);
+        myLinkedList.append(10);
+        myLinkedList.append(11);
+        myLinkedList.append(12);
+        // myLinkedList.removeFirst();
+        // Node deletedNode = myLinkedList.removeLast();
+        // System.err.println("DeletedNode: " + deletedNode.value);
         myLinkedList.printList("abc");
-        myLinkedList.insert(1, 23);
-        myLinkedList.insert(1, 38);
-        myLinkedList.insert(0, 49);
-        int len = myLinkedList.getLength();
-        System.err.println("length: " + len);
+        // myLinkedList.insert(1, 23);
+        // myLinkedList.insert(1, 38);
+        // myLinkedList.insert(0, 49);
+        // int len = myLinkedList.getLength();
+        // System.err.println("length: " + len);
+        // myLinkedList.printList("xyz");
+        // Node node = myLinkedList.getNode(1); // Fetch the node at a particular index
+        // System.out.println("Node: " + node.value);
+        // Node removedNode = myLinkedList.remove(2);
+        // System.err.println("Removed Node: " + removedNode.value);
+        // myLinkedList.reverse();
+        // Node node = myLinkedList.findMiddleNode();
+        // System.err.println("Node: " + node.value);
+        // Node node = myLinkedList.findKthFromEnd(9);
+        // System.err.println("Node: " + node);
+        // myLinkedList.removeDuplicates();
+        // int num = myLinkedList.binaryToDecimal();
+        // System.err.println("num: "+ num);
+        // myLinkedList.partitionList(5);
+        // myLinkedList.reverseBetween(1, 4);
+        myLinkedList.swapPairs();
         myLinkedList.printList("xyz");
-        Node node = myLinkedList.getNode(1); // Fetch the node at a particular index
-        System.out.println("Node: " + node.value);
     }
 }
--- Day 02-Linked List/linkedlist/LinkedList.java
+++ Day 02-Linked List/linkedlist/LinkedList.java
@@ -1,5 +1,8 @@
 package linkedlist;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinkedList {
 
     public Node head;
@@ -142,7 +145,6 @@ public class LinkedList {
 
     public int removeFirst() {
         if (head == null) {
-            System.err.println("Invalid action!! Linked list is already empty");
             return 0;
         } else if (length == 1) {
             int val = head.value;
@@ -185,4 +187,203 @@ public class LinkedList {
         }
         return null;
     }
+
+    // Remove a node at a particular index
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        } else if (index == 0) {
+            Node temp = head;
+            removeFirst();
+            return temp;
+        } else {
+            Node temp = head;
+            Node pre = head;
+            for (int i = 0; i < length; i++) {
+                if (i == index) {
+                    Node temp2 = temp;
+                    pre.next = temp.next;
+                    temp.next = null;
+                    length--;
+                    return temp2;
+                }
+                pre = temp;
+                temp = temp.next;
+            }
+            return null;
+        }
+    }
+
+    public void reverse() {
+        if (head == null) {
+            System.err.println("Linked list is empty");
+            return;
+        }
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after;
+        Node before = null;
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
+
+    public Node findMiddleNode() {
+        if (head == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast == null) {
+                break;
+            }
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    public Node findKthFromEnd(int k) {
+        if (head == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        for (int i = 1; i < k; i++) {
+            fast = fast.next;
+            if (fast == null) {
+                return null;
+            }
+        }
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    // Using nexted loops instead of using hashSet.
+    // This method is not efficient as hashSet because nexted loops have time complexity O(n^m) where m is the number of nexted loops. In this case the time complexity is O(n^2).
+    // public void removeDuplicates() {
+    //     Node current = head;
+    //     Node runner;
+    //     while (current != null) {
+    //         runner = current;
+    //         while (runner.next != null) {
+    //             if (runner.next.value == current.value) {
+    //                 System.err.println("runner: " + runner.next.value + ", current: " + current.value);
+    //                 runner.next = runner.next.next;
+    //                 length--;
+    //             } else {
+    //                 runner = runner.next;
+    //             }
+    //         }
+    //         current = current.next;
+    //     }
+    // }
+
+    // Using hashSet
+    // This method is more efficient on the basis of time complexity which is O(n) in this case.
+    public void removeDuplicates() {
+        Set<Integer> values = new HashSet<>();
+        Node pre = null;
+        Node current = head;
+
+        while (current != null) {
+            if (values.contains(current.value)) {
+                pre.next = current.next;
+                length--;
+            } else {
+                values.add(current.value);
+                pre = current;
+            }
+            current = current.next;
+        }
+    }
+
+    public int binaryToDecimal() {
+        int decimalValue = 0;
+        Node current = head;
+        while (current != null) {
+            decimalValue *= 2;
+            decimalValue += current.value;
+            current = current.next;
+        }
+        return decimalValue;
+    }
+
+    public void partitionList(int n) {
+        if (head == null) {
+            return;
+        }
+        Node current = head;
+        Node dummy1 = new Node(0); // for smaller value nodes
+        Node dummy2 = new Node(0); // for larger value nodes
+        Node prev1 = dummy1;
+        Node prev2 = dummy2;
+
+        while (current != null) {
+            if (current.value < n) {
+                // Insert the node in dummy1
+                prev1.next = current;
+                prev1 = prev1.next;
+            } else {
+                // Insert the node in dummy2
+                prev2.next = current;
+                prev2 = prev2.next;
+            }
+            current = current.next;
+        }
+
+        prev1.next = dummy2.next;
+        head = dummy1.next;
+        prev2.next = null;
+    }
+
+    public void reverseBetween(int startIndex, int endIndex) {
+        if (head == null) {
+            return;
+        }
+
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+        Node previousNode = dummyNode;
+        for (int i = 0; i < startIndex; i++) {
+            previousNode = previousNode.next;
+        }
+        Node currentNode = previousNode.next;
+        for (int i = 0; i < endIndex - startIndex; i++) {
+            Node nodeToMove = currentNode.next;
+            currentNode.next = nodeToMove.next;
+            nodeToMove.next = previousNode.next;
+            previousNode.next = nodeToMove;
+        }
+
+        head = dummyNode.next;
+    }
+
+    public void swapPairs() {
+        Node dummy = new Node(0);
+        dummy.next = head;
+
+        Node previous = dummy;
+        Node first = head;
+
+        while (first != null && first.next != null) {
+            Node second = first.next;
+            previous.next = second;
+            first.next = second.next;
+            second.next = first;
+            previous = first;
+            first = first.next;
+        }
+        head = dummy.next;
+    }
+
 }
